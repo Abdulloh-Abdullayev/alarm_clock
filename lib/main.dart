e26e1d9e6e4a62fdb1adb5861890205877767df0
Abdulloh-Abdullayev <abdullohabdullayev321@gmail.com>
2022-07-22 09:21:37 +0300

    new function added

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- lib/pages/alarm_clock.dart
+++ lib/pages/alarm_clock.dart
@@ -1,35 +1,59 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
+import 'package:time_picker_sheet/widget/sheet.dart';
+import 'package:time_picker_sheet/widget/time_picker.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime dateTimeSelected = DateTime.now();
+
+  void _openTimePickerSheet(BuildContext context) async {
+    final result = await TimePicker.show<DateTime?>(
+      context: context,
+      sheet: TimePickerSheet(
+        sheetTitle: 'Select meeting schedule',
+        minuteTitle: 'Minute',
+        hourTitle: 'Hour',
+        saveButtonText: 'Save',
+      ),
+    );
+
+    if (result != null) {
+      setState(() {
+        dateTimeSelected = result;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("Alarm Clock"),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ZoomIn(
-              child: SizedBox(
-                child: Image(
-                  image: AssetImage("assets/images/img.png"),fit: BoxFit.cover,
-                ),
-              ),
-            )
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => _openTimePickerSheet(context),
+              child: Text('show time picker sheet'),
+            ),
+            Text('Eslatma qo\'yilgan voxt ${dateTimeSelected.hour}:${dateTimeSelected.minute}'),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          FlutterAlarmClock.createAlarm(23, 12, title: "Uyg'oning Oka!");
+          FlutterAlarmClock.createAlarm(dateTimeSelected.hour, dateTimeSelected.minute, title: "Uyg'oning Oka!");
         },
         child: Icon(Icons.alarm),
       ),
